44a1e138b141ecdb12fa3006e5f8f75c7205eed4
vegarn13 <vegard_nerland@hotmail.com>
2015-01-26 12:48:17 +0100

    roman_to_int fulført (burde pyntes på)

--- lab2.py
+++ lab2.py
@@ -9,41 +9,47 @@ print roman_number.upper()
 
 
 def roman_to_int(romIn):
-	runBool = False #runBool blir satt til True hvis input verdien er gyldig
+	runBool = True #runBool blir satt til True hvis input verdien er gyldig
 	romIn = romIn.upper() #sett alle bokstavenen til uppercase
 	value_list = [] #list til bokstavenes tall verdi
 	sum = 0
 	for char in romIn: #check om input verdi er gyldig
-		if char == 'I'or char == 'V' or char == 'X' or char == 'L' or char == 'C' or char == 'D' or char == 'M':
-			runBool = True
-	for char in romIn: #legger bokstavenes tall verdi i value_list
-		if char == 'M':
-			value_list.append(1000)
-		elif char == 'D':
-			value_list.append(500)
-		elif char == 'C':
-			value_list.append(100)
-		elif char == 'L':
-			value_list.append(50)
-		elif char == 'X':
-			value_list.append(10)
-		elif char == 'V':
-			value_list.append(5)
-		elif char == 'I':
-			value_list.append(1)
-	print value_list
+		if char != 'I'or char == 'V' or char == 'X' or char == 'L' or char == 'C' or char == 'D' or char == 'M':
+			runBool = False
 	
-	for i, item in enumerate(value_list):
-		print "index: %s, item: %s" %(i, item)
-		if item < value_list[i+1]:
-			sum -= item
-		else:
-			sum += item
+	if runBool == True:
+		for char in romIn: #legger bokstavenes tall verdi i value_list
+			if char == 'M':
+				value_list.append(1000)
+			elif char == 'D':
+				value_list.append(500)
+			elif char == 'C':
+				value_list.append(100)
+			elif char == 'L':
+				value_list.append(50)
+			elif char == 'X':
+				value_list.append(10)
+			elif char == 'V':
+				value_list.append(5)
+			elif char == 'I':
+				value_list.append(1)
+		print value_list
 		
-	print sum
+		for i, item in enumerate(value_list):
+			print "index: %s, item: %s" %(i, item)
+			try:
+				if item < value_list[i+1]:
+					sum -= item
+				else:
+					sum += item
+			except:
+				runBool = False
+	else:
+		print "Ikke gyldig tall"
+		return sum
 	print runBool
 
-roman_to_int(roman_number)
+print roman_to_int(roman_number)
 
 
 """Titall til Romertall"""
