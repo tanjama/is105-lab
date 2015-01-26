4bb9a430efdd47e3a7d44f652f14ec0598958801
vegarn13 <vegard_nerland@hotmail.com>
2015-01-26 13:03:41 +0100

    bugfix og komentarer

--- lab2.py
+++ lab2.py
@@ -3,20 +3,22 @@
 
 print 'Begynn med Bokstav med Høyest verdi til venstre så nest høyest til høyre for den også videre'
 print 'hver bokstav kan ha et negativt tall på venstre side'
-print 'eks: 4 = IV, 3 = VIII, 9 = IX, 45 = VL'
+print 'eks: 4 = IV, 7 = VIII, 9 = IX, 45 = VL'
 roman_number = raw_input("skriv inn rommertall med bokstaver mellom I og M: ")
-print roman_number.upper()
 
 
 def roman_to_int(romIn):
 	runBool = True #runBool blir satt til True hvis input verdien er gyldig
 	romIn = romIn.upper() #sett alle bokstavenen til uppercase
 	value_list = [] #list til bokstavenes tall verdi
+	char_list = []
 	sum = 0
 	for char in romIn: #check om input verdi er gyldig
-		if char != 'I'or char == 'V' or char == 'X' or char == 'L' or char == 'C' or char == 'D' or char == 'M':
+		if char == 'I'or char == 'V' or char == 'X' or char == 'L' or char == 'C' or char == 'D' or char == 'M':
+			char_list.append(char)
+		else:
 			runBool = False
-	
+	print char_list
 	if runBool == True:
 		for char in romIn: #legger bokstavenes tall verdi i value_list
 			if char == 'M':
@@ -35,7 +37,7 @@ def roman_to_int(romIn):
 				value_list.append(1)
 		print value_list
 		
-		for i, item in enumerate(value_list):
+		for i, item in enumerate(value_list): # ser etter negative verdier og legger samme i sum variabel
 			print "index: %s, item: %s" %(i, item)
 			try:
 				if item < value_list[i+1]:
@@ -43,11 +45,10 @@ def roman_to_int(romIn):
 				else:
 					sum += item
 			except:
-				runBool = False
-	else:
-		print "Ikke gyldig tall"
+				print "done"
 		return sum
-	print runBool
+	else:
+		return "Ikke gyldig tall"
 
 print roman_to_int(roman_number)
 
