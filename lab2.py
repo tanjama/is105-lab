ba3e055f0eafff4074b0c97c8bbcf2ea524e4838
vegarn13 <vegard_nerland@hotmail.com>
2015-02-12 11:27:00 +0100

    merge

--- lab2.py
+++ lab2.py
@@ -2,22 +2,27 @@
 """Romertall til Titall
 Funksjonen tar inn et romertall mellom I og M og gir ut et tall i titallssystemet(desimalltall)"""
 
-#Kode-blokk til Demo i console
-print 'Bokstav med høyest verdi settes til venstre. Deretter settes tall etter høyest verdi mot høyre.'
-print 'I stedet for desimal rangering (1,2,3,4,5 osv) brukes romersk gradering (1000, 500, 100, 50, 10 osv).'
-print 'Hver bokstav kan ha et negativt tall på venstre side. Dvs at det negative tallet trekkes fra det postive tallet som er stilt til høyre.'
-print 'eks: 4 = IV(5 - 1), 7 = VII(5 + 1 + 1) , 9 = IX (10 - 1), 45 = XLV(50 - 10 + 5)'
-roman_number = raw_input("Skriv inn rommertall med bokstaver mellom I og M: ")
-
-
+"""
+def char_check(romIn_check ):
+	for char in romIn: #Check om input-verdi er gyldig
+			if not char == 'I' or char == 'V' or char == 'X' or char == 'L' or char == 'C' or char == 'D' or char == 'M':
+				print char
+			else:
+				return False
+"""
 def roman_to_int(romIn):
 	runBool = True #runBool blir satt til True hvis input-verdien er gyldig.
 	romIn = romIn.upper() #Setter alle bokstavenen til uppercase.
 	value_list = [] #Liste til bokstavenes(romertallenes) tall-verdi.
-	char_list = []
 	sum = 0
-	runBool = char_check(romIn)
-	print char_list
+	print romIn
+	print runBool
+	for char in romIn: #Check om input-verdi er gyldig
+		if char == 'I' or char == 'V' or char == 'X' or char == 'L' or char == 'C' or char == 'D' or char == 'M':
+			print char
+		else:
+			runBool = False
+	print runBool	
 	if runBool == True:
 		for char in romIn: #Legger bokstavenes tall-verdi i value_list.
 			if char == 'M':
@@ -50,13 +55,11 @@ def roman_to_int(romIn):
 	else:
 		return "Not valid number"
 
-print roman_to_int(roman_number)
 
 
 """Titall til Romertall
 Funksjonen tall et desimalt tall og konverterer det til et tall med romerske symbol(romertall).
 """
-number_in_raw = raw_input("Write a number: ")# Her skrives et ønsket titall inn. 
 
 def int_to_roman(number_in_string): #Funksjon som skal gjøre titall om til romertall. 
 	number_in = int(number_in_string)
@@ -120,14 +123,21 @@ def int_to_roman(number_in_string): #Funksjon som skal gjøre titall om til rome
 			result.append("I")
 			
 	return "".join(result) #Setter resultatene sammen. I stedet for at reslutatet blir slik: 'M' 'D' 'C' 'L' 'X' 'V' 'I' blir det slik: MDCLXVI
-print int_to_roman(number_in_raw) #Resultetet (de romerske symbolene) blir printet. 
 
 """Addisjon av romertall"""
 
 def roman_plus_roman(rom_num1, rom_num2):
 	runBool = True
-	runBool = char_check(rom_num1)
-	runBool = char_check(rom_num2)
+	for char in rom_num1: #Check om input-verdi er gyldig
+		if char == 'I' or char == 'V' or char == 'X' or char == 'L' or char == 'C' or char == 'D' or char == 'M':
+			print char
+		else:
+			runBool = False
+	for char in rom_num2: #Check om input-verdi er gyldig
+		if char == 'I' or char == 'V' or char == 'X' or char == 'L' or char == 'C' or char == 'D' or char == 'M':
+			print char
+		else:
+			runBool = False
 	
 	if runBool == True:
 		rom_num1 = convert(rom_num1)
@@ -141,7 +151,8 @@ def roman_plus_roman(rom_num1, rom_num2):
 					rom_num_result.append(item)
 		
 		return(revert("".join(rom_num_result)))
-
+	else:
+		return "not valid input"
 def revert(rom_num):
 	rom_num = rom_num.replace("DCCCC", "CM")
 	rom_num = rom_num.replace("CCCC", "CD")
@@ -160,40 +171,20 @@ def convert(rom_num):
 	rom_num = rom_num.replace("IV", "IIII")
 	return rom_num
 
-def char_check(romIn):	
-	for char in romIn: #Check om input-verdi er gyldig
-			if char == 'I'or char == 'V' or char == 'X' or char == 'L' or char == 'C' or char == 'D' or char == 'M':
-				char_list.append(char)
-			else:
-				return False
+
+#Kode-blokk til Demo i console
+print 'Bokstav med høyest verdi settes til venstre. Deretter settes tall etter høyest verdi mot høyre.'
+print 'I stedet for desimal rangering (1,2,3,4,5 osv) brukes romersk gradering (1000, 500, 100, 50, 10 osv).'
+print 'Hver bokstav kan ha et negativt tall på venstre side. Dvs at det negative tallet trekkes fra det postive tallet som er stilt til høyre.'
+print 'eks: 4 = IV(5 - 1), 7 = VII(5 + 1 + 1) , 9 = IX (10 - 1), 45 = XLV(50 - 10 + 5)'
+roman_number = raw_input("Skriv inn rommertall med bokstaver mellom I og M: ")
+print roman_to_int(roman_number)
+
+number_in_raw = raw_input("Write a number: ")# Her skrives et ønsket titall inn. 
+print int_to_roman(number_in_raw) #Resultetet (de romerske symbolene) blir printet. 
 
 rom1 = raw_input("input a valid roman numeral: ")
 rom2 = raw_input("input a valid roman numeral: ")
-
 print roman_plus_roman(rom1, rom2)
+
 raw_input("")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
