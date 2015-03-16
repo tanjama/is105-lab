a2f4895732818d0be57b6f3ba5cc2ac14a46c2f7
orsnes <thomasorsnes@gmail.com>
2015-03-16 12:40:37 +0100

    Update Lab6.py

--- Lab6.py
+++ Lab6.py
@@ -8,7 +8,7 @@ b)
 Get = Den ber om en represtasjon av en spesefik resurs. forspørselen som bruker GET skal bare motta data, og ikke ha noen andre effekter.
 Connection = Den konverterer forsespørselen om tilkobling til en åpenbar/gjennomsiktig TCP/IP tunnel. målet med dette er for å legge til rette for SSL kryptert kommunkasjon(HTTPS) gjennom en ukrypter HTTP server.
 Host = Det er siden vi henter informasjon fra eller den siden vi er på i dette tilfelle er det http://www-net.cs.umass.edu/
-Accept =
-Origin =
+Accept = Accept førespørselen brukes til å spesifisere hvilke mediatyper som er akseptable for responsen fra hosten. Accept forespørselen kan også inneholde parametere som setter en standard for kvaliteten på innholdet.
+Origin = Denne headeren kommer fra brukersiden, og beskriver hvilke sikkherhetskontekster som gjorde at brukeren startet en http forespørsel. Serversiden kan bruke origin headeren for å forsikre seg om at brukerens nettleser kan stoles på.
 Accept Encoding =
 Accept Language =  
