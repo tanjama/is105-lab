75ce0777e65b475372915c7fa76daa20a628996f
Tanja Malbasa <tanja.malbasa@live.no>
2015-03-16 13:13:48 +0100

    .

--- Lab6.py
+++ Lab6.py
@@ -51,12 +51,43 @@ som vil utstede en http-respons. Så verdien brukes til å regne ut hvor lang ti
 tar fra http-klienten utsteder en forespørsel til en http-server og får en http-respons. 
 
 
+Kilder, lastet ned 16.03.2015: 
+http://www.w3.org/Protocols/rfc2616/rfc2616-sec14.html#sec14.11 w3.org
+https://ask.wireshark.org/questions/13384/display-http-header wireshark.org
+https://wiki.wireshark.org/Hyper_Text_Transfer_Protocol wireshark.org
+https://www.youtube.com/watch?v=NHLTa29iovU Youtube.com
+
+
 b)
-Get = Den ber om en represtasjon av en spesefik resurs. forspørselen som bruker GET skal bare motta data, og ikke ha noen andre effekter.
-Connection = Den konverterer forsespørselen om tilkobling til en åpenbar/gjennomsiktig TCP/IP tunnel. målet med dette er for å legge til rette for SSL kryptert kommunkasjon(HTTPS) gjennom en ukrypter HTTP server.
-Host = Det er siden vi henter informasjon fra eller den siden vi er på i dette tilfelle er det http://www-net.cs.umass.edu/
-Accept = Accept førespørselen brukes til å spesifisere hvilke mediatyper som er akseptable for responsen fra hosten. Accept forespørselen kan også inneholde parametere som setter en standard for kvaliteten på innholdet.
-Origin = Denne headeren kommer fra brukersiden, og beskriver hvilke sikkherhetskontekster som gjorde at brukeren startet en http forespørsel. Serversiden kan bruke origin headeren for å forsikre seg om at brukerens nettleser kan stoles på.
-Accept Encoding = Accept Encoding forespørsel-header feltet er lik som Accept, men begrenser innholdet i koden som er akseptert for en tilbakemelding.  Når en server tester om koden er akseptabel i forhold til Accept Encoding bruker man følgene regler : 1. Hvis koden er listet i Accept Encoding feltet er dette akseptabelt, men hvis qvalue = 0 er dette ikke akseptabelt. 2. Symbolet «*» i et Accept Encoding felt matcher alle tilgjengelige koder og må ikke være listet i header feltet. 3. Hvis flere koder er akseptable, blir den koden som har høyest verdi fra qvalue 1+ valgt. 4. «Identity» innholds koden er alltid akseptabel, hvis den ikke spesifikt inneholder «identity;q=0», eller fordi feltet inkluderer «*q=0». Hvis Accept Encoding feltets verdi er tom er bare selve «identity» akseptabel. 
+Get = Den ber om en represtasjon av en spesefik resurs. 
+Forspørselen som bruker GET skal bare motta data, og ikke ha noen andre effekter.
+Connection = Den konverterer forsespørselen om tilkobling til en 
+åpenbar/gjennomsiktig TCP/IP tunnel. 
+Målet med dette er for å legge til rette for SSL kryptert 
+kommunkasjon(HTTPS) gjennom en ukrypter HTTP server.
+
+Host = Det er siden vi henter informasjon fra eller den siden 
+vi er på i dette tilfelle er det http://www-net.cs.umass.edu/.
+
+Accept = Accept førespørselen brukes til å spesifisere hvilke 
+mediatyper som er akseptable for responsen fra hosten. 
+Accept forespørselen kan også inneholde parametere som setter 
+en standard for kvaliteten på innholdet.
+
+Origin = Denne headeren kommer fra brukersiden, og beskriver hvilke 
+sikkherhetskontekster som gjorde at brukeren startet en http forespørsel. 
+Serversiden kan bruke origin headeren for å forsikre seg om 
+at brukerens nettleser kan stoles på.
+
+Accept Encoding = Accept Encoding forespørsel-header feltet er lik som Accept, 
+men begrenser innholdet i koden som er akseptert for en tilbakemelding.
+Når en server tester om koden er akseptabel i forhold til Accept Encoding 
+bruker man følgene regler : 1. Hvis koden er listet i Accept Encoding feltet er 
+dette akseptabelt, men hvis qvalue = 0 er dette ikke akseptabelt. 2. Symbolet «*» 
+i et Accept Encoding felt matcher alle tilgjengelige koder og må ikke være listet 
+i header feltet. 3. Hvis flere koder er akseptable, blir den koden som har høyest 
+verdi fra qvalue 1+ valgt. 4. «Identity» innholds koden er alltid akseptabel, 
+hvis den ikke spesifikt inneholder «identity;q=0», eller fordi feltet inkluderer «*q=0». 
+Hvis Accept Encoding feltets verdi er tom er bare selve «identity» akseptabel. 
 Accept Language = Accept Language forespørsel-header feltet er lik som Accept, men begrenser språkene som er foretrukket som en respons av forespørselen. 
  
