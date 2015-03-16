fb7acf4d976d13b8f9f44448d756353ffc1afcca
Stanley Ntiamoah <stan-93@hotmail.com>
2015-03-16 12:11:22 +0100

    Create Lab6.py

---
+++ Lab6.py
@@ -0,0 +1,14 @@
+Lab 6 
+
+Oppgave 1
+
+a) HTTP Get meldingen ble sendt ut etter 3.171428000 sekunder, og vi mottok HTTP Get Ok etter 3.189186000 sekunder. Det vil si at det tok 0.017758 sekunder fra da HTTP Get meldingen ble sendt ut til vi mottok HTTP GET Ok meldingen.
+
+b)
+Get = Den ber om en represtasjon av en spesefik resurs. forspørselen som bruker GET skal bare motta data, og ikke ha noen andre effekter.
+Connection = Den konverterer forsespørselen om tilkobling til en åpenbar/gjennomsiktig TCP/IP tunnel. målet med dette er for å legge til rette for SSL kryptert kommunkasjon(HTTPS) gjennom en ukrypter HTTP server.
+Host = Det er siden vi henter informasjon fra eller den siden vi er på i dette tilfelle er det http://www-net.cs.umass.edu/
+Accept =
+Origin =
+Accept Encoding =
+Accept Language =  
